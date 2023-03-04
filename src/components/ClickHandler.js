9832e5f01fbc2542168640edb1b6dab5dc19257e
Jaswal1p <Jaswal1p@yahoo.com>
2023-03-04 17:45:46 -0500

    added code for additional mouse events

--- src/components/ClickHandler.js
+++ src/components/ClickHandler.js
@@ -1,14 +1,15 @@
 export const ClickHandler = () => {
-    const ClickHandler = (e, count =1) => {
+    const clickHandler = (e, count =1) => {
         console.log('You clicked me!', count, e);
-        alert('You clicked me!');
+        // alert('You clicked me!');
         
     }
    return (
       <div>
-        <button onClick={ClickHandler}>Click Me</button>
-        <button onClick={(e) => ClickHandler(e, 5)}>Click 5</button>
-        
+        <button onClick={clickHandler}>Click Me</button>  
+        <button onClick={(e) => clickHandler(e, 5)}>Click 5</button>
+        <button onDoubleClick={(e) => clickHandler(e, 6)}>Double Click Here 6</button>
+        <button onMouseEnter={(e) => clickHandler(e, 7)}>Mouse enter Here 7</button>
       </div>
    )
 };
